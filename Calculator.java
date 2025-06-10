421e54cffbfa3d8c3a324ee687d6dcc4ec464708
Naveen2138 <naveenkumarvajrad04@gmail.com>
2025-06-10 08:49:09 +0530

    savefile

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+ public class Calculator {
+    public static int add(int a, int b) { return a + b; }
+    public static int subtract(int a, int b) { return a - b; }
+    public static int multiply(int a, int b) { return a * b; }
+    public static double divide(int a, int b) {
+        return (b == 0) ? 0 : (double) a / b;
+    }
+    public static void performOperations() {
+        int a = 10, b = 5;
+        System.out.println("Add: " + add(a, b));
+        System.out.println("Subtract: " + subtract(a, b));
+        System.out.println("Multiply: " + multiply(a, b));
+        System.out.println("Divide: " + divide(a, b));
+    }
+    public static void main(String[] args) {
+        performOperations();
+    }
+ }
